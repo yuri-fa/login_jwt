eed90aaaef9abe525ab5885f473ffb6dc35986b9
yuri.alcantara <yuri.alcantara@INOTE983.iteris.local>
2021-06-24 21:56:32 -0300

    dev : insert roles in autentication;

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -45,9 +45,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll()
-				.antMatchers(HttpMethod.GET, "/actuator").permitAll().antMatchers(HttpMethod.GET, "/actuator/*")
-				.permitAll().anyRequest().authenticated().and().csrf().disable().sessionManagement()
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+				.antMatchers(HttpMethod.POST, "/auth").permitAll()
+				.antMatchers(HttpMethod.GET, "/actuator").permitAll()
+				.antMatchers(HttpMethod.GET, "/actuator/*").permitAll()
+				.antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
+				.anyRequest().authenticated().and().csrf().disable().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository),
 						UsernamePasswordAuthenticationFilter.class);
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$zM9S6uE5STUSOaKhLZaZ/udFTXZOvPzFSdIi5W/2FWsGfY.OXJMkq');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$zM9S6uE5STUSOaKhLZaZ/udFTXZOvPzFSdIi5W/2FWsGfY.OXJMkq');
+
+INSERT INTO PERFIL(id,nome) VALUES(1,'ROLE_ALUNO');
+INSERT INTO PERFIL(id,nome) VALUES(2,'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id,perfis_id) VALUES(1,1);
+INSERT INTO USUARIO_PERFIS(usuario_id,perfis_id) VALUES(2,2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
