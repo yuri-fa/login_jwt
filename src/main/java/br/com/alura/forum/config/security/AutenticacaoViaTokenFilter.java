1b84fdff168780693fba502914e41faa64d4b3af
yuri.alcantara <yuri.alcantara@host.docker.internal>
2021-07-06 16:39:48 -0300

    reverte para deploy em jar, configurações de properties com variaveis de ambiente, configuracao do dockerfile;

--- src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
